85666c2d2cba92c7e245a7d8b2b431879649f08c
vleras <vleras512@gmail.com>
2024-12-23 17:38:02 +0200

    images

---
+++ src/pages/ImageCarousel.jsx
@@ -0,0 +1,50 @@
+import React, { useState, useRef } from "react";
+import "./Carousel.css"; // Add custom styles for carousel
+
+const Carousel = ({ images }) => {
+  const [isDragging, setIsDragging] = useState(false);
+  const [startX, setStartX] = useState(0);
+  const [scrollLeft, setScrollLeft] = useState(0);
+  const carouselRef = useRef(null);
+
+  const handleMouseDown = (e) => {
+    setIsDragging(true);
+    setStartX(e.pageX - carouselRef.current.offsetLeft);
+    setScrollLeft(carouselRef.current.scrollLeft);
+  };
+
+  const handleMouseMove = (e) => {
+    if (!isDragging) return;
+    e.preventDefault();
+    const x = e.pageX - carouselRef.current.offsetLeft;
+    const walk = x - startX; // Distance dragged
+    carouselRef.current.scrollLeft = scrollLeft - walk;
+  };
+
+  const handleMouseUp = () => {
+    setIsDragging(false);
+  };
+
+  const handleMouseLeave = () => {
+    setIsDragging(false);
+  };
+
+  return (
+    <div
+      className="carousel"
+      ref={carouselRef}
+      onMouseDown={handleMouseDown}
+      onMouseMove={handleMouseMove}
+      onMouseUp={handleMouseUp}
+      onMouseLeave={handleMouseLeave}
+    >
+      {images.map((img, index) => (
+        <div className="carousel-item" key={index}>
+          <img src={img} alt={`Slide ${index}`} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Carousel;
---
+++ src/pages/carouselData.js
@@ -0,0 +1,12 @@
+// src/data/carouselData.js
+
+// Import all images
+import image1 from "../images/bdtronic_slide.png";
+import image2 from "../images/image2.png";
+import image3 from "../images/image3.png";
+import image4 from "../images/image4.png";
+import image5 from "../images/image5.png";
+import image6 from "../images/image6.png";
+
+// Export the array of images
+export const images = [image1, image2, image3, image4, image5, image6];
